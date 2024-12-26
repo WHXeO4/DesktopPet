25e477a9ca3ebc7b72c61f429a58eeecb7e08c10
WHXeO4 <141139727+WHXeO4@users.noreply.github.com>
2024-12-26 20:14:59 +0800

    Add files via upload

---
+++ DeskTopPet/src/start.bat
@@ -0,0 +1,8 @@
+@echo off
+if "%1" == "h" goto begin
+mshta vbscript:createobject("wscript.shell").run("""%~nx0"" h",0)(window.close)&&exit
+:begin
+REM
+D:
+cd python/DeskTopPet/src
+python main.py
